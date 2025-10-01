e7cebd283e52b5fa9bf7d0cdea78d58b330844ad
julia.bot <julia.fajardo0111@gmail.com>
2025-10-01 19:13:53 +0800

    update: add admin & accountant side nav; add temporary radio buttons in sign in page

--- fit4school-web/src/App.jsx
+++ fit4school-web/src/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import AAccMod from './a_pages/a_acc_mod/a_acc_mod.jsx';
+import ADashboard from './a_pages/a_dashboard/a_dashboard.jsx';
+import AcDashboard from './a_pages/ac_dashboard/ac_dashboard.jsx';
 
-export default function App(){
+function App(){
   return(
-    <div>
-      <AAccMod />
-    </div>
-  )
+    <>
+      <Routes>
+        <Route path="/" element={<AAccMod />} />
+        <Route path="/a_dashboard" element={<ADashboard />} />
+        <Route path="/ac_dashboard" element={<AcDashboard />} />
+      </Routes>
+    </>
+  );
 
 }
+
+export default App;
--- fit4school-web/src/a_pages/a_acc_mod/a_acc_mod.jsx
+++ fit4school-web/src/a_pages/a_acc_mod/a_acc_mod.jsx
@@ -1,30 +1,46 @@
 import React from 'react'
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import eyeIcon from '../../assets/icons/eye.svg';
 import eyeOffIcon from '../../assets/icons/eye-closed.svg';
 
 const AAccMod = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
+  const [role, setRole] = useState('admin');
+
+ const handleSignIn = (e) => {
+  e.preventDefault();
+  
+  if (role === 'admin') {
+    navigate('/a_dashboard');
+  } else {
+    navigate('/ac_dashboard');
+  }
+};
+
   return (
     <div className="flex min-h-screen">
-      {/* Left side - Green */}
+      {/* Left side */}
       <div className="flex-1 bg-green-500 flex flex-col items-center justify-center text-white">
         <h3 className="font-bold text-1xl">WELCOME TO</h3>
         <h1 className="font-bold text-3xl">FIT4SCHOOL</h1>
       </div>
 
-      {/* Right side - White with form */}
+      {/* Right side with form */}
       <div className="flex-1 bg-white flex items-center justify-center">
-        <form className="w-80 bg-white p-6">
+        
+        <form className="w-80 bg-white p-6" onSubmit={handleSignIn}>
           <h2 className="text-2xl font-bold mb-10 text-gray-800 text-center">Sign In</h2>
           <input
-            className="w-full border p-2 border-gray-500 mb-3 rounded"
+            className="w-full border p-2 border-gray-300 mb-3 rounded-lg"
             type="email"
             placeholder="Email"
           />
+
           <div className="relative mb-3">
             <input
-              className="w-full border p-2  border-gray-500 rounded pr-10"
+              className="w-full border p-2  border-gray-300 rounded-lg pr-10"
               type={showPassword ? "text" : "password"}
                placeholder="Password"
           />
@@ -47,12 +63,39 @@ const AAccMod = () => {
           </label>
           
           <button type="button"
-            className="text-blue-500 hover:bold text-sm"
+            className="text-blue-500 hover:underline text-sm"
             onClick={() => alert("Forgot password clicked")}>Forgot Password?
           </button>
         </div>
+
+      {/* this is a temporary radio button */}
+       <div className="mb-4">
+        <p className="text-sm mb-2 text-gray-700">Sign in as:</p>
+        <div className="flex gap-4">
+          <label className="flex items-center accent-green-600">
+            <input 
+              type="radio" 
+              value="admin"
+              checked={role === 'admin'}
+              onChange={(e) => setRole(e.target.value)}
+              className="mr-2"
+            />
+            Admin
+          </label>
+          <label className="flex items-center accent-green-600">
+            <input 
+              type="radio" 
+              value="accountant"
+              checked={role === 'accountant'}
+              onChange={(e) => setRole(e.target.value)}
+              className="mr-2"
+            />
+            Accountant
+          </label>
+        </div>
+      </div>
         
-          <button className="font-bold w-full bg-blue-400 text-white p-2 rounded hover:bg-blue-600">
+          <button type="submit" className="font-bold w-full bg-blue-400 text-white p-2 rounded-lg hover:bg-blue-600">
             SIGN IN
           </button>
         </form>
