e85d85ccbdaeba4b46439e48cb8bc71f05ff8455
unknown <dhenizelopez@gmail.com>
2025-10-05 13:38:10 +0800

    UPDATE: Added Fixes, WIP: AR Debugging and Error Handling

--- fit4school-mob/app/index.jsx
+++ fit4school-mob/app/index.jsx
@@ -1,7 +1,13 @@
 import { Redirect } from "expo-router";
 
 export default function Index() {
-    return <Redirect href="dash_mod/home" />; 
-    // HOMESCREEN: dash_mod/home | CURRENTLY WORKING: ar_mod/ar_height, ar_mod/ar_result, items_mod/boys_unif_p | MAIN SCREEN DISPLAY, just change it when debugging
+    return <Redirect href="dash_mod/home" />; //MAIN SCREEN DISPLAY, just change it when debugging
+    
+    //JEANNEN'S DEVELOPMENT PART
+    // HOMESCREEN: dash_mod/home | CURRENTLY WORKING: acc_mod/landing
+
+
+    //DHENIZE'S DEVELOPMENT PART
+    // HOMESCREEN: dash_mod/home | CURRENTLY WORKING: ar_mod/ar_height, ar_mod/ar_result, items_mod/boys_unif_p
 }
 
